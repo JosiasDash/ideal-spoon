a4e4e06505760f007eb660feebea7ec034540ff0
JosiasDH <hanniel.degbelo@epitech.eu>
2025-02-09 22:39:36 +0100

    Add Bootstrap and dotenv support; update .gitignore and Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {config} from "dotenv"
 
+config();
+
+// import.meta.env
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    define: {
+        'process.env': process.env,
+    }
 })
